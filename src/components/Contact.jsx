9542a5c9389996d55415e8e73186716a380ad10f
Maulik Vaghela <vaghelamaulik679@gmail.com>
2026-07-17 10:22:44 +0530

    Installed react router

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      <ul>
+        <li>Email : maulikv123@gmail.com</li>
+        <li>LinkedIn</li>
+        <li>GitHub</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,14 @@
+function Projects() {
+  return (
+    <div>
+      <h2>Projects</h2>
+      <ul>
+        <li>Portfolio Website</li>
+        <li>Parking App</li>
+        <li>Student Management System</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Projects;
